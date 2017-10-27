5bbea443926bb3108052bf05a2e59eb4883771ac
Elkin V.G <elkinvg@gmail.com>
2017-10-27 16:53:14 +0300

    init

---
+++ app/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', []).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
