c9b09bec382c925f2cc35d80e7d5a05438e82f04
Elkin V.G <elkinvg@gmail.com>
2017-11-17 14:42:54 +0300

    attribute

--- app/log_viewer_app_user.js
+++ app/log_viewer_app_user.js
@@ -29,18 +29,35 @@ app.controller('tableCtrl',  function ($scope, $http, $controller, uiGridConstan
           });
 
           function DialogController($scope, $mdDialog, row) {
+            // Входящие данные в колонках
             // command_json - команда в JSON с аргументами
             // command_name - имя команды
             // description - описание источника
             // device_name - имя девайса
             // timestamp - время вызова команды
             // user_ip - ip юзера
+            // type_req - тип запроса
+            // is_command - это команда или запись атрибута
+            // status - статус выполнения запроса
             $scope.dataFromRow = row.entity;
+            if ($scope.dataFromRow.is_command) {
+              $scope.dataFromRow.class_comm_or_attr = "iscommand";
+            }
+            else {
+              $scope.dataFromRow.class_comm_or_attr = "isattr";
+            }
 
-            var commandJson = angular.fromJson($scope.dataFromRow.command_json);
+            if (angular.isArray($scope.dataFromRow.argin)) {
+                var outargin = "["
+                angular.forEach($scope.dataFromRow.argin, function(data) {
+                    outargin+=" ";
+                    outargin+=data;
+                    outargin+=" ";
+                });
+                outargin += "]";
+                $scope.dataFromRow.argin = outargin;
+            }
 
-            $scope.dataFromRow.command_name = commandJson.command;
-            $scope.dataFromRow.argin = commandJson.argin;
         
             // Просто закрыть диалоговое окно
             $scope.cancel = function() {
@@ -59,10 +76,25 @@ app.controller('tableCtrl',  function ($scope, $http, $controller, uiGridConstan
               cellFilter: 'date:"dd-MM-yyyy HH:mm:ss"'
           },
             // { field: 'device_name', displayName: 'Имя девайса'},
-            { field: 'command_name', displayName: 'Команда', width: '15%', cellTemplate : '<div ng-dblclick="grid.appScope.commandClicked(row,col)" class="ui-grid-cell-contents">{{COL_FIELD CUSTOM_FILTERS}}<md-tooltip class="ng-tool-tip">Кликните дважды для получения дополнительной информации</md-tooltip></div>'},
+            { 
+              field: 'command_name', 
+              // displayName: '<b>Команда</b>', 
+              width: '15%',
+              headerCellTemplate: '<div class="ui-grid-cell-contents" col-index="renderIndex"><span class="iscommand">Команда</span> / <span class="isattr">Атрибут</span></div>',
+              cellTemplate : '<div ng-dblclick="grid.appScope.commandClicked(row,col)" class="ui-grid-cell-contents">{{COL_FIELD CUSTOM_FILTERS}}<md-tooltip class="ng-tool-tip">Кликните дважды для получения дополнительной информации</md-tooltip></div>',
+              // Для выставления стиля в зависимости от типа данных
+              cellClass: function(grid, row) 
+              {
+                if (row.entity.is_command) {
+                  return 'iscommand';
+                }
+                return 'isattr';
+              }
+            },
+              
             // { field: 'alias', displayName: 'Псевдоним', width: '10%'},
             { field: 'user_ip', displayName: 'IP', width: '8%'},
-            { field: 'description', displayName: 'Источник'}
+            { field: 'description', displayName: 'Адресат команды'}
         ],
         paginationPageSizes: [25, 50, 75, 100],
         paginationPageSize: 25
